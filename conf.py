4118ce0ef58b44d89d8b830b431c79e2f5c955a3
Javious <javioustlj@outlook.com>
2019-05-27 00:38:31 +0800

    support math

--- conf.py
+++ conf.py
@@ -58,3 +58,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+mathjax_config = {
+    'extensions': ['tex2jax.js'],
+    'jax': ['input/TeX', 'output/HTML-CSS'],
+}
